dcb48db4cf747f513c8e8df1f0c7df8ba92397e1
Minmul <68465075+minmul07@users.noreply.github.com>
2023-03-26 19:34:48 +0900

    main4.kt Kotlin에서 함수 유형 및 람다 표현식 사용 solution code

--- main4.kt
+++ main4.kt
@@ -1,11 +1,23 @@
 fun main() {
-    val coins: (Int) -> String = { quantity ->
-        "$quantity quarters"
-    }
+    /*
+    val coins: (Int) -> String = {
+        "$it quarters"
+    } 사용안함. 람다 표현식을 함수에 직접 전달하였음*/
 
-    val treatFunction = trickOrTreat(false, coins)
+    /*
+    val coins: (Int) -> String = { quantity ->
+            "$quantity quarters"
+    }*/
+
+    val treatFunction = trickOrTreat(false, { "$it quarters" })
+    //val treatFunction = trickOrTreat(false) { "$it quarters" } 위와 같음. 후행 람다 문법
     val trickFunction = trickOrTreat(true, null)
     treatFunction()
+    trickFunction()
+
+    repeat(4) {
+        treatFunction()
+    }
     trickFunction()
 }
 
