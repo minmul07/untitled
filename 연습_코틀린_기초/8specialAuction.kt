0812ff45e88642933b05c6056962c993e51208b5
minmul07 <sksmstmdrud@gmail.com>
2023-03-28 22:42:36 +0900

    8. 특별 경매

---
+++ 연습_코틀린_기초/8specialAuction.kt
@@ -0,0 +1,17 @@
+fun main() {
+    val winningBid = Bid(5000, "Private Collector")
+
+    println("Item A is sold at ${auctionPrice(winningBid, 2000)}.")
+    println("Item B is sold at ${auctionPrice(null, 3000)}.")
+}
+
+class Bid(val amount: Int, val bidder: String)
+
+fun auctionPrice(bid: Bid?, minimumPrice: Int): Int {
+    return bid?.amount ?: minimumPrice
+}
+
+/*
+Item A is sold at 5000.
+Item B is sold at 3000.
+ */
