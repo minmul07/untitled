010d61f9dfe46a895607ef8e22f6c11bcebc5cde
minmul07 <sksmstmdrud@gmail.com>
2023-03-28 22:01:34 +0900

    6. 인터넷 프로필

---
+++ 연습_코틀린_기초/6internetProfile.kt
@@ -0,0 +1,40 @@
+fun main() {
+    val amanda = Person("Amanda", 33, "play tennis", null)
+    val atiqah = Person("Atiqah", 28, "climb", amanda)
+
+    amanda.showProfile()
+    atiqah.showProfile()
+}
+
+class Person(val name: String, val age: Int, val hobby: String?, val referrer: Person?) {
+    fun showProfile() {
+        println("Name: $name")
+        println("Age: $age")
+        if (hobby != null) {
+            println("Likes to $hobby. ")
+        }
+
+        if (referrer != null) {
+            print("Has a referrer named ${referrer.name}")
+            if (referrer.hobby != null) {
+                print(", who likes to ${referrer.hobby}")
+            } else {
+                print(".")
+            }
+        } else  {
+            print("Doesn't have a referrer.")
+        }
+        println("\n")
+    }
+}
+
+
+/*
+Name: Amanda
+Age: 33
+Likes to play tennis. Doesn't have a referrer.
+
+Name: Atiqah
+Age: 28
+Likes to climb. Has a referrer named Amanda, who likes to play tennis.
+ */
